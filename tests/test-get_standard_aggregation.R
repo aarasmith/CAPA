b5c2f709f6cf7673bf51b1c4e9087b3ad094ede2
aarasmith <andyarasmith@gmail.com>
2022-09-09 14:20:34 +0200

    2022/09/09 - Work PC - renamed to proper name

--- tests/test-get_standard_aggregation.R
+++ tests/test-get_standard_aggregation.R
@@ -1,9 +1,9 @@
 
-test_weights <- list(L25 = 0, L50 = 0, L100 = 0, M25 = 0, M50 = 0, M100 = 0, H25 = 0, H50 = 0, H100 = 0,
+test_weights <- list(L25 = 0, L50 = 0, L100 = 0, M25 = 0, M50 = 1, M100 = 0, H25 = 0, H50 = 0, H100 = 0,
                      int25 = 1, int50 = 1, int100 = 0)
 
 
-test_that("std_agg runs", {
+test_that("std_agg full test", {
   
   world_yearly_length <- length(ison("World")) * 32
   
