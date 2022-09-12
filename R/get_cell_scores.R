a4862571e899c46e44e5c5bf59766dbbfe2b2acc
aarasmith <andyarasmith@gmail.com>
2022-09-12 16:49:53 +0200

    forgot to save before commit

--- R/get_cell_scores.R
+++ R/get_cell_scores.R
@@ -2,8 +2,33 @@
 
 
 
-get_cell_scores <- function(iso, years, start_end = c(1,12), weights, draw_adm1 = TRUE, draw_points = TRUE){
-  #browser()
+get_cell_scores <- function(iso, years, weights, start_end = c(1,12), draw_adm1 = TRUE, draw_points = TRUE){
+  
+  #' Query the CAPA database for cell-scores aggregation
+  #' 
+  #' @description Writes and sends an SQL query to the CAPA db based on user supplied inputs
+  #' 
+  #' @usage get_cell_scores(iso, years, weights, start_end = c(1,12), draw_adm1 = TRUE, draw_points = TRUE)
+  #' 
+  #' @param iso character/numeric: a single or vector of numeric iso3n values or character iso3c values
+  #' @param years numeric: a single or vector containing a range of years
+  #' @param weights list: a list of named weight elements containing a single whole number value
+  #' @param start_end numeric: vector of length 2 with the 2 numbers corresponding with the start month of the first year and end
+  #' month of the final year
+  #' @param draw_adm1 boolean: If TRUE will attach ADM1 geometry to the output list under out_list$adm and will be plotted when given to plot_cell_scores()
+  #' @param draw_points boolean: If TRUE will attach GED events to the output list under out_list$events and will be plotted when given to plot_cell_scores()
+  #' 
+  #' @examples 
+  #' get_cell_scores(iso = 760, 2019:2020, test_weights, start_end = c(2, 11))
+  #' get_cell_scores(c("SYR", "AFG"), 2020, test_weights, start_end = c(1, 12))
+  #'                          
+  #' @returns list length 3 of sf dataframes. If draw_adm1 or draw_points are FALSE, they will be sf dataframes of nrow = 0
+  #' 
+  #' @details 
+  #' Dependencies: dplyr, sf
+  #' Uses: sanitize_iso(), connect_to_capa(), query_cell_scores(), cnames(), disconnect_from_capa()
+  #' Used-with: plot_cell_scores()
+  
   iso3n <- sanitize_iso(iso)
   
   capa_db <- connect_to_capa()
--- tests/test-get_cell_scores.R
+++ tests/test-get_cell_scores.R
@@ -1,18 +1,18 @@
 source("../R/get_cell_scores.R")
 
-#x <- get_cell_scores(iso, years, start_end = c(1,12), weights, draw_adm1 = TRUE, draw_points = TRUE)
+#x <- get_cell_scores(iso, years, weights, start_end = c(1, 12), draw_adm1 = TRUE, draw_points = TRUE)
 
 test_that("get_cell_scores", {
   
-  test_weights <- list(L25 = 0, L50 = 0, L100 = 0, M25 = 0, M50 = 1, M100 = 0, H25 = 0, H50 = 0, H100 = 0,
+  test_weights<- list(L25 = 0, L50 = 0, L100 = 0, M25 = 0, M50 = 1, M100 = 0, H25 = 0, H50 = 0, H100 = 0,
                        int25 = 1, int50 = 1, int100 = 0)
   capa_db <- connect_to_capa()
   
   case_list <- list(
-    get_cell_scores(760, 2020, start_end = c(1,12), test_weights),
-    get_cell_scores(760, 2019:2020, start_end = c(2,11), test_weights),
-    get_cell_scores(c(760, 368), 2020, start_end = c(1,12), test_weights),
-    get_cell_scores("USA", 2020, start_end = c(1,12), test_weights)
+    get_cell_scores(760, 2020, test_weights, start_end = c(1, 12)),
+    get_cell_scores(760, 2019:2020, test_weights, start_end = c(2, 11)),
+    get_cell_scores(c(760, 368), 2020, test_weights, start_end = c(1, 12)),
+    get_cell_scores("USA", 2020, test_weights, start_end = c(1, 12))
   )
   
   lapply(case_list, function(case){expect_s3_class(case$cells, "sf")})
