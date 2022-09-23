b57873dc456a4f9e37cb24e4c50d11c9cd6c6bf5
aarasmith <andyarasmith@gmail.com>
2022-09-23 11:49:37 +0200

    2022/09/23 - Work PC - sourcing for server compatibility

--- app.R
+++ app.R
@@ -1,16 +1,18 @@
 
 source("global.R")
-source("R/mod_info.R")
-source("R/mod_weights.R")
-source("R/mod_exposure.R")
-source("R/mod_exposure_map.R")
-source("R/mod_score_map.R")
-source("R/mod_frequency.R")
-source("R/mod_regional.R")
-source("R/mod_car.R")
-source("R/util_modules/mod_download.R")
+# source("R/mod_info.R")
+# source("R/mod_weights.R")
+# source("R/mod_exposure.R")
+# source("R/mod_exposure_map.R")
+# source("R/mod_score_map.R")
+# source("R/mod_frequency.R")
+# source("R/mod_regional.R")
+# source("R/mod_car.R")
+# source("R/util_modules/mod_download.R")
 source("mod_server.R")
 source("mod_ui.R")
+
+lapply(list.files("R", recursive = T, full.names = T), source)
 
 
 shinyApp(ui = ui, server = server)
